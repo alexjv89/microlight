24a6fc1c115d1c5a8440eb5f3fb5aa806b454995
Alex J V <alexjv89@gmail.com>
2024-04-09 12:45:46 +0530

    missed this in the previous commit

--- api/services/GeneralService.js
+++ api/services/GeneralService.js
@@ -209,4 +209,16 @@ module.exports = {
 		// returns Edit or Create - notice the caps
 	},
 	getAllFiles:getAllFiles,
+	// removes leading spaces in lines in markdown
+	// makes it possible to write multi like markdown in a json object which is intended at any level
+	cleanMD:function(md){
+		if(!md)
+			md='<empty>';
+		var lines = md.split('\n');
+		var trimmed_lines=[];
+		lines.forEach(function(line){
+			trimmed_lines.push(line.trim())
+		})
+		return trimmed_lines.join('\n');
+	}
 };
